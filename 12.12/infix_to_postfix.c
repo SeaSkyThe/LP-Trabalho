1e2a8934ed066614f51496fd05b26c0105e93e15
SeaSkyThe <marceloer2011@gmail.com>
2019-05-22 16:26:19 -0300

    12.12 Finalizado.

---
+++ 12.12/infix_to_postfix.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "stack.h"
+
+
+//Functions
+int isOperator(char c){
+    if(c == '+' || c == '-' || c == '/' || c == '*' || c == '^' || c == '%'){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int precedence(char operator1, char operator2){
+    if(operator1 == '^'){
+        if(operator2 == '^'){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+    else if(operator1 == '*' || operator1 == '/'){
+        if(operator2 == '^'){
+            return -1;
+        }
+        else if(operator2 == '*' || operator2 == '/'){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+    }
+    else if(operator1 == '+' || operator1 == '-'){
+        if(operator2 == '^' || operator2 == '*' || operator2 == '/'){
+            return -1;
+        }
+        else if(operator2 == '+' || operator2 == '-'){
+            return 0;
+        }
+    }
+}
+
+void convertToPostfix(char infix[], char postfix[]){
+    struct stack *op_parenthesis = (struct stack*)malloc(sizeof(struct stack));  //Operations and parenthesis
+    start_stack(op_parenthesis);
+    push(op_parenthesis, '(');
+    strcat(infix,")");
+    int count_postfix = 0;
+
+    while(!isEmpty(op_parenthesis)){
+        for(int i = 0; *(infix + i) != '\0'; i++){
+            if(!isOperator(*(infix + i)) && *(infix + i) != '(' && *(infix + i) != ')'){
+                *(postfix + count_postfix) = *(infix + i);
+                count_postfix++;
+            }
+            else if(*(infix + i) == '('){
+                push(op_parenthesis, '(');
+            }
+            else if(isOperator(*(infix + i))){
+                while(isOperator(op_parenthesis -> top -> data) && (precedence(op_parenthesis -> top -> data, *(infix + i))) != -1){
+                    *(postfix + count_postfix) = pop(op_parenthesis);
+                    count_postfix++;
+                }
+                push(op_parenthesis, *(infix + i));
+            }
+            else if(*(infix + i) == ')'){
+                while(op_parenthesis -> top -> data != '('){
+                    *(postfix + count_postfix) = pop(op_parenthesis);
+                    count_postfix++;
+                }
+                pop(op_parenthesis);
+            }
+        }
+    }
+}
+
+int main(void){
+    char infix[100] = "6+2*5-8/4";
+    char postfix[100] = {0};
+
+    printf("\n\nInfix: \n");
+    for(int i = 0; infix[i] != '\0'; i++){
+        printf("%c", infix[i]);
+    }
+
+
+    convertToPostfix(infix, postfix);
+
+    printf("\n\nPostFix: \n");
+    for(int i = 0; postfix[i] != '\0'; i++){
+        printf("%c", postfix[i]);
+    }
+    printf("\n\n");
+    return 0;
+}
